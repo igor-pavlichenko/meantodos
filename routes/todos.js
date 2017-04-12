9323149fb0c81426c645a0fd7b6e0078f2252f8c
Igor <ig0r260892@gmail.com>
2017-04-13 00:08:41 +0100

    Implement route to acquire all todos

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://ng-meantodos-user:ng-meantodos-user@ds159880.mlab.com:59880/meantodos', ['todos'])
 
+// Get todos route
 router.get('/todos', function (request, response, next) {
-	response.send('TODOS API');
+	db.todos.find(function (err, todos) {
+		if (err) {
+			response.send(err);
+		} else {
+			response.json(todos);
+		}
+	});
 });
 
 module.exports = router;
