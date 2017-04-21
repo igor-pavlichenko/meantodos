1dcbf943ccdc3faae13513d1f934823abb9d60cb
Igor <ig0r260892@gmail.com>
2017-04-21 02:43:58 +0100

    Implement a todo service and add a todo model class
    Use todo service to get todos in todos ocmponent.

--- client/src/app/components/todos/todos.component.ts
+++ client/src/app/components/todos/todos.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TodoService } from '../../services/todo.service';
+
+import { Todo } from '../../models/todo'
+
 @Component({
 	selector: 'todos',
 	templateUrl: './todos.component.html',
 	styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
+	todos: Todo[];
 
-	constructor() { }
+	constructor(private todoService: TodoService) { }
 
 	ngOnInit() {
+		this.todos = [];
+		this.todoService.getTodos()
+			.subscribe(todos => {
+				console.log(todos);
+			})
 	}
 
 }
